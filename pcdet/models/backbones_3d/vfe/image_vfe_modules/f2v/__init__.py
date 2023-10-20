1d20dac0202e28003b430b9d0890510f0672fc80
Inyoung Lee <69599501+2in0@users.noreply.github.com>
2023-10-20 16:12:13 +0900

    Error correction

--- pcdet/models/backbones_3d/vfe/image_vfe_modules/f2v/__init__.py
+++ pcdet/models/backbones_3d/vfe/image_vfe_modules/f2v/__init__.py
@@ -1,9 +1,7 @@
 from .frustum_to_voxel import FrustumToVoxel
 from .frustum_to_voxel_with_point import FrustumToVoxelWithPoint
-from .voxel_field_fusion import VoxelFieldFusion
 
 __all__ = {
     'FrustumToVoxel': FrustumToVoxel,
     'FrustumToVoxelWithPoint': FrustumToVoxelWithPoint,
-    'VoxelFieldFusion': VoxelFieldFusion,
 }
